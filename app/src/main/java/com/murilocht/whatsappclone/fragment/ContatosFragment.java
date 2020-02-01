94832b4604a227450dc060526d23cdb1201b79c2
murilocht <murilo980ti@outlook.com>
2020-02-01 11:52:22 -0300

    parando na aula 41

--- app/src/main/java/com/murilocht/whatsappclone/fragment/ContatosFragment.java
+++ app/src/main/java/com/murilocht/whatsappclone/fragment/ContatosFragment.java
@@ -3,30 +3,92 @@ package com.murilocht.whatsappclone.fragment;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 import com.murilocht.whatsappclone.R;
+import com.murilocht.whatsappclone.adapter.ContatosAdapter;
+import com.murilocht.whatsappclone.config.ConfiguracaoFirebase;
+import com.murilocht.whatsappclone.model.Usuario;
 
-/**
- * A simple {@link Fragment} subclass.
- */
+import java.util.ArrayList;
+
 public class ContatosFragment extends Fragment {
 
+    private RecyclerView recyclerViewListaContatos;
+    private ContatosAdapter adapter;
+    private ArrayList<Usuario> listaContatos = new ArrayList<>();
+    private DatabaseReference usuariosRef;
+    private ValueEventListener valueEventListenerContatos;
 
     public ContatosFragment() {
-        // Required empty public constructor
+
     }
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contatos, container, false);
+        View view = inflater.inflate(R.layout.fragment_contatos, container, false);
+        recyclerViewListaContatos = view.findViewById(R.id.recyclerViewListaContatos);
+        usuariosRef = ConfiguracaoFirebase.getFirebaseDatabase().child("usuarios");
+
+        adapter = new ContatosAdapter(listaContatos, getActivity());
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        recyclerViewListaContatos.setLayoutManager(layoutManager);
+        recyclerViewListaContatos.setHasFixedSize(true);
+        recyclerViewListaContatos.setAdapter(adapter);
+
+        return view;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        recuperarContatos();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        usuariosRef.removeEventListener(valueEventListenerContatos);
+
+        listaContatos.clear();
+        adapter.notifyDataSetChanged();
+    }
+
+    public void recuperarContatos() {
+        valueEventListenerContatos = usuariosRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                listaContatos.clear();
+
+                for (DataSnapshot dados : dataSnapshot.getChildren()) {
+                    Usuario usuario = dados.getValue(Usuario.class);
+                    listaContatos.add(usuario);
+                }
+
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 
 }
